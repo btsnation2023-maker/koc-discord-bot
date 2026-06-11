5bb2f9c5f76103273c9bcecd0e30d93949e14b8c
btsnation2023-maker <btsnation2023@gmail.com>
2026-06-11 12:51:08 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const http = require('http'); const PORT = process.env.PORT || 3000; http.createServer((req,res)=>{res.writeHead(200);res.end('alive');}).listen(PORT);
+require('dotenv').config()
 const { Client, GatewayIntentBits, ActionRowBuilder, ButtonBuilder, ButtonStyle, ModalBuilder, TextInputBuilder, TextInputStyle, EmbedBuilder } = require('discord.js');
 const { google } = require('googleapis');
 
